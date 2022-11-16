e941e39cdd6959604d5ad00c2b3ab89af0ca85fe
Sebastian <sebastian.dziadzio@gmail.com>
2022-11-16 20:03:51 +0100

    Add a setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="codis",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires="~=3.9",
+    author="Sebastian Dziadzio",
+    author_email="dziadzio@hey.com",
+)
