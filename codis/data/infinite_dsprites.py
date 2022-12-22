7ebe77e8eb318d2c69b6a0dfa4ebf8a2eff5db5f
Sebastian <sebastian.dziadzio@gmail.com>
2022-12-22 14:16:58 +0100

    Add first attempt at shape generation

--- codis/data/infinite_dsprites.py
+++ codis/data/infinite_dsprites.py
@@ -1,46 +1,85 @@
-import cmath
-from math import atan2, pi
-from random import random
-from matplotlib import pyplot as plt
 import numpy as np
-from scipy.interpolate import splprep, splev
+from matplotlib import pyplot as plt
+from scipy.interpolate import splev, splprep
 
 
-def convex_hull(pts):
-    xleftmost, yleftmost = min(pts)
-    by_theta = [(atan2(x - xleftmost, y - yleftmost), x, y) for x, y in pts]
-    by_theta.sort()
-    as_complex = [complex(x, y) for _, x, y in by_theta]
-    hull = as_complex[:2]
-    for pt in as_complex[2:]:
-        # Perp product.
-        while ((pt - hull[-1]).conjugate() * (hull[-1] - hull[-2])).imag < 0:
-            hull.pop()
-        hull.append(pt)
-    return [(pt.real, pt.imag) for pt in hull]
+def plot_shapes_on_grid(n=3):
+    """Plot an n x n grid of random shapes."""
+    _, axes = plt.subplots(
+        nrows=n, ncols=n, figsize=(15, 15), layout="tight", subplot_kw={"aspect": 1.0}
+    )
+    for ax in axes.flat:
+        verts, spline = generate_shape(
+            min_verts=4, max_verts=10, radius_std=0.5, angle_std=0.5
+        )
+        ax.axis("off")
+        ax.scatter(verts[0], verts[1], label="vertices", color="blue")
+        ax.scatter([0], [0], label="origin", color="green")
+        ax.plot(spline[0], spline[1], label="spline", color="red")
+    plt.savefig("shapes.png")
 
 
-def interpolate_spline(points):
-    points = np.array(points).T
-    print(points.shape)
-    tck, u = splprep(points, u=None, s=0.0, per=1)
-    u_new = np.linspace(u.min(), u.max(), 1000)
-    x, y = splev(u_new, tck, der=0)
+def generate_shape(
+    min_verts: int = 3,
+    max_verts: int = 10,
+    radius_std: float = 0.5,
+    angle_std: float = 0.5,
+):
+    """Generate a random polygon and optionally interpolate it with a spline.
+    Args:
+        min_verts: Minimum number of vertices (inclusive).
+        max_verts: Maximum number of vertices (inclusive).
+        radius_std: Standard deviation of the polar radius when sampling the vertices.
+        angle_std: Standard deviation of the polar angle when sampling the vertices.
+    Returns:
+        A tuple of (points, spline), where points is an array of points of shape (2, num_verts)
+        and spline is an array of shape (2, num_spline_points).
+    """
+    n = np.random.randint(min_verts, max_verts + 1)
+    verts = sample_vertices(n, radius_std, angle_std)
+    spline = interpolate(verts) if np.random.rand() < 0.5 else interpolate(verts, k=1)
+    return verts, spline
+
+
+def sample_vertices(n: int, radius_std: float, angle_std: float):
+    """Sample polar coordinates of the vertices.
+    Args:
+        n: Number of vertices.
+        radius_std: Standard deviation of the polar radius when sampling the vertices.
+        angle_std: Standard deviation of the polar angle when sampling the vertices.
+    Returns:
+        An array of shape (2, num_verts).
+    """
+    radia = np.random.normal(1.0, radius_std, n)
+    radia = np.clip(radia, 0.1, 1.9)
+
+    sector = 2 * np.pi / n
+    intervals = np.linspace(0, 2 * np.pi, n, endpoint=False)
+    angles = np.random.normal(0.0, sector / 2 * angle_std, n)
+    angles = np.clip(angles, -sector / 2, sector / 2) + intervals
+
+    points = [
+        [radius * np.cos(angle), radius * np.sin(angle)]
+        for radius, angle in zip(radia, angles)
+    ]
+    return np.array(points).T
+
+
+def interpolate(verts, k: int = 3, num_spline_points: int = 1000):
+    """Interpolate a set of vertices with a spline.
+    Args:
+        verts: An array of shape (2, num_verts).
+        k: The degree of the spline.
+        num_spline_points: The number of points to sample from the spline.
+    Returns:
+        An array of shape (2, num_spline_points).
+    """
+    verts = np.column_stack((verts, verts[:, 0]))
+    spline_params, u = splprep(verts, s=0.0, per=0, k=k)
+    u_new = np.linspace(u.min(), u.max(), num_spline_points)
+    x, y = splev(u_new, spline_params, der=0)
     return np.array([x, y])
 
 
-def generate_shape(n=6):
-    points = [(random() + 0.8) * cmath.exp(2j * pi * i / 7) for i in range(n)]
-    points = convex_hull([(p.real, p.imag) for p in points])
-    points = interpolate_spline(points)
-    return points
-
-
-def main():
-    points = generate_shape()
-    plt.plot(points[0], points[1], label="spline", color="red")
-    plt.show()
-
-
 if __name__ == "__main__":
-    main()
+    plot_shapes_on_grid()
