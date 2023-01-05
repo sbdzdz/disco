0a2b6b8837aef54f580c6a49f83591caff0e60fe
Sebastian <sebastian.dziadzio@gmail.com>
2023-01-05 11:52:16 +0100

    Move the visualization to a separate module

--- codis/data/infinite_dsprites.py
+++ codis/data/infinite_dsprites.py
@@ -1,28 +1,7 @@
 import numpy as np
 import pygame
-from matplotlib import pyplot as plt
 import pygame.gfxdraw
 from scipy.interpolate import splev, splprep
-
-
-def plot_shapes_on_grid(n=8):
-    """Plot an n x n grid of random shapes.
-    Args:
-        n: The number of rows and columns in the grid.
-    Returns:
-        None
-    """
-    _, axes = plt.subplots(
-        nrows=n, ncols=n, figsize=(20, 20), layout="tight", subplot_kw={"aspect": 1.0}
-    )
-    for ax in axes.flat:
-        verts, spline = generate_shape(
-            min_verts=4, max_verts=10, radius_std=0.5, angle_std=0.7
-        )
-        ax.axis("off")
-        ax.scatter(verts[0], verts[1], label="vertices", color="blue")
-        ax.plot(spline[0], spline[1], label="spline", color="red")
-    plt.savefig("shapes.png")
 
 
 def draw_shape(scale: int = 100, position: tuple[int] = (80, 80)):
