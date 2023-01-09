be4414fad4838d58b6621680f9ef62adfe16a4f6
Sebastian <sebastian.dziadzio@gmail.com>
2023-01-09 22:43:04 +0100

    Add an animation showcasing the dataset

--- codis/data/visualization.py
+++ codis/data/visualization.py
@@ -1,8 +1,14 @@
+import io
+from itertools import islice
+
+import imageio.v2 as imageio
 from matplotlib import pyplot as plt
-from codis.data.infinite_dsprites import generate_shape
+from tqdm import tqdm
+
+from codis.data.infinite_dsprites import InfiniteDSprites
 
 
-def plot_shapes_on_grid(n=8):
+def plot_shapes_on_grid(n=5):
     """Plot an n x n grid of random shapes.
     Args:
         n: The number of rows and columns in the grid.
@@ -12,15 +18,33 @@ def plot_shapes_on_grid(n=8):
     _, axes = plt.subplots(
         nrows=n, ncols=n, figsize=(20, 20), layout="tight", subplot_kw={"aspect": 1.0}
     )
+    dataset = InfiniteDSprites()
     for ax in axes.flat:
-        verts, spline = generate_shape(
-            min_verts=4, max_verts=10, radius_std=0.5, angle_std=0.7
-        )
+        verts, spline = dataset.generate_shape()
         ax.axis("off")
         ax.scatter(verts[0], verts[1], label="vertices", color="blue")
         ax.plot(spline[0], spline[1], label="spline", color="red")
     plt.savefig("shapes.png")
 
 
+def zoom_out_vis(n=9, num_frames=100):
+    """Create an animated GIF showing a grid of n x n InfiniteDSprites datasets."""
+    datasets = [iter(InfiniteDSprites()) for _ in range(n**2)]
+    frames = [[next(dataset) for dataset in datasets] for _ in range(num_frames)]
+    _, axes = plt.subplots(
+        nrows=n, ncols=n, figsize=(20, 20), layout="tight", subplot_kw={"aspect": 1.0}
+    )
+    with imageio.get_writer("zoom_out.gif", mode="I") as writer:
+        for frame in tqdm(frames):
+            for ax, (image, _) in zip(axes.flat, frame):
+                ax.axis("off")
+                ax.imshow(image)
+                buffer = io.BytesIO()
+            plt.savefig(buffer, format="png")
+            image = imageio.imread(buffer)
+            writer.append_data(image)
+            plt.cla()
+
+
 if __name__ == "__main__":
-    plot_shapes_on_grid()
+    zoom_out_vis()
