f00c75b54dfe303ddc6527e230335579d32aabbd
Sebastian <sebastian.dziadzio@gmail.com>
2023-01-12 20:30:39 +0100

    Unify the API for both plotting functions

--- codis/data/visualization.py
+++ codis/data/visualization.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 from codis.data.infinite_dsprites import InfiniteDSprites
 
 
-def plot_shapes_on_grid(n=5):
+def plot_shapes_on_grid(nrows=5, ncols=12):
     """Plot an n x n grid of random shapes.
     Args:
         n: The number of rows and columns in the grid.
@@ -15,7 +15,11 @@ def plot_shapes_on_grid(n=5):
         None
     """
     _, axes = plt.subplots(
-        nrows=n, ncols=n, figsize=(20, 20), layout="tight", subplot_kw={"aspect": 1.0}
+        nrows=nrows,
+        ncols=ncols,
+        figsize=(ncols / nrows * 10, 10),
+        layout="tight",
+        subplot_kw={"aspect": 1.0},
     )
     dataset = InfiniteDSprites()
     for ax in axes.flat:
@@ -26,17 +30,24 @@ def plot_shapes_on_grid(n=5):
     plt.savefig("shapes.png")
 
 
-def plot_animated_shapes_on_grid(nrows=5, ncols=12, num_frames=10, height=10):
-    """Create an animated GIF showing a grid of n x n InfiniteDSprites datasets."""
+def plot_animated_shapes_on_grid(nrows=5, ncols=12, num_frames=100):
+    """Create an animated GIF showing a grid of nrows x ncols InfiniteDSprites datasets.
+    Args:
+        nrows: The number of rows in the grid.
+        ncols: The number of columns in the grid.
+        num_frames: The number of frames in the animation.
+        height: The height of the grid in inches.
+    Returns:
+        None
+    """
     datasets = [iter(InfiniteDSprites(image_size=128)) for _ in range(nrows * ncols)]
     frames = [[next(dataset) for dataset in datasets] for _ in range(num_frames)]
-    aspect_ratio = ncols / nrows
     with imageio.get_writer("zoom_out.gif", mode="I") as writer:
         for frame in tqdm(frames):
             _, axes = plt.subplots(
                 nrows,
                 ncols,
-                figsize=(aspect_ratio * height, height),
+                figsize=(ncols / nrows * 10, 10),
                 layout="tight",
                 subplot_kw={"aspect": 1.0},
             )
