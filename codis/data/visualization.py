42c064b4a8f4f7aec4711b0bb68a3912ed8643e3
Sebastian <sebastian.dziadzio@gmail.com>
2023-01-12 19:27:02 +0100

    Update method names, move the subplot inside the loop to avoid jitter

--- codis/data/visualization.py
+++ codis/data/visualization.py
@@ -1,5 +1,4 @@
 import io
-from itertools import islice
 
 import imageio.v2 as imageio
 from matplotlib import pyplot as plt
@@ -20,36 +19,36 @@ def plot_shapes_on_grid(n=5):
     )
     dataset = InfiniteDSprites()
     for ax in axes.flat:
-        verts, spline = dataset.generate_shape()
+        verts, spline = dataset.sample_polygon()
         ax.axis("off")
         ax.scatter(verts[0], verts[1], label="vertices", color="blue")
         ax.plot(spline[0], spline[1], label="spline", color="red")
     plt.savefig("shapes.png")
 
 
-def zoom_out_vis(nrows=5, ncols=12, num_frames=300, height=10):
+def plot_animated_shapes_on_grid(nrows=5, ncols=12, num_frames=10, height=10):
     """Create an animated GIF showing a grid of n x n InfiniteDSprites datasets."""
-    datasets = [iter(InfiniteDSprites()) for _ in range(nrows * ncols)]
+    datasets = [iter(InfiniteDSprites(image_size=128)) for _ in range(nrows * ncols)]
     frames = [[next(dataset) for dataset in datasets] for _ in range(num_frames)]
     aspect_ratio = ncols / nrows
-    _, axes = plt.subplots(
-        nrows,
-        ncols,
-        figsize=(aspect_ratio * height, height),
-        layout="tight",
-        subplot_kw={"aspect": 1.0},
-    )
     with imageio.get_writer("zoom_out.gif", mode="I") as writer:
         for frame in tqdm(frames):
+            _, axes = plt.subplots(
+                nrows,
+                ncols,
+                figsize=(aspect_ratio * height, height),
+                layout="tight",
+                subplot_kw={"aspect": 1.0},
+            )
             for ax, (image, _) in zip(axes.flat, frame):
                 ax.axis("off")
                 ax.imshow(image)
                 buffer = io.BytesIO()
             plt.savefig(buffer, format="png")
+            plt.close()
             image = imageio.imread(buffer)
             writer.append_data(image)
-            plt.cla()
 
 
 if __name__ == "__main__":
-    zoom_out_vis()
+    plot_animated_shapes_on_grid()
