766e783b7beeb05b280d406c2368b3b449821a33
Sebastian <sebastian.dziadzio@gmail.com>
2023-02-12 16:17:10 +0100

    Make Encoder and Decoder available as blocks

--- codis/models/autoencoder.py
+++ codis/models/autoencoder.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from torch import nn
 
-from codis.models.blocks import build_decoder_block, build_encoder_block
+from codis.models.blocks import Encoder, Decoder
 
 
 class AutoEncoder(nn.Module):
@@ -16,20 +16,10 @@ class AutoEncoder(nn.Module):
 
         if hidden_dims is None:
             hidden_dims = [32, 64, 128, 256, 512]
-
         hidden_dims = [in_channels] + hidden_dims
-        encoder_modules = [
-            build_encoder_block(in_channels, out_channels)
-            for in_channels, out_channels in zip(hidden_dims[:-1], hidden_dims[1:])
-        ]
-        self.encoder = nn.Sequential(*encoder_modules)
 
-        hidden_dims.reverse()
-        decoder_modules = [
-            build_decoder_block(in_channels, out_channels)
-            for in_channels, out_channels in zip(hidden_dims[:-1], hidden_dims[1:])
-        ]
-        self.decoder = nn.Sequential(*decoder_modules)
+        self.encoder = Encoder(in_channels, hidden_dims)
+        self.decoder = Decoder(reversed(hidden_dims))
 
     def forward(self, x):
         """Forward pass of the autoencoder."""
--- codis/models/blocks.py
+++ codis/models/blocks.py
@@ -2,70 +2,84 @@
 from torch import nn
 
 
-def build_encoder_block(
-    in_channels: int,
-    out_channels: int,
-    kernel_size: int = 3,
-    stride: int = 2,
-    padding: int = 1,
-) -> nn.Sequential:
-    """Build an encoder block."""
-    return nn.Sequential(
-        nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride,
-            padding,
-        ),
-        nn.BatchNorm2d(out_channels),
-        nn.LeakyReLU(),
-    )
+class Encoder(nn.Module):
+    """A simple encoder model."""
+
+    def __init__(self, hidden_dims: list[int]) -> None:
+        """Initialize the encoder.
+        Args:
+            in_channels: The number of input channels.
+            hidden_dims: The number of channels in each hidden layer.
+        Returns:
+            None
+        """
+        super().__init__()
+        module = [
+            nn.Sequential(
+                nn.Conv2d(
+                    in_channels,
+                    out_channels,
+                    kernel_size=3,
+                    stride=2,
+                    padding=1,
+                ),
+                nn.BatchNorm2d(out_channels),
+                nn.LeakyReLU(),
+            )
+            for in_channels, out_channels in zip(hidden_dims[:-1], hidden_dims[1:])
+        ]
+        self.encoder = nn.Sequential(*module)
+
+    def forward(self, x):
+        """Forward pass of the encoder."""
+        x = self.encoder(x)
+        return x
 
 
-def build_decoder_block(
-    in_channels: int,
-    out_channels: int,
-    kernel_size: int = 3,
-    stride: int = 2,
-    padding: int = 1,
-    output_padding: int = 1,
-) -> nn.Sequential:
-    """Build a decoder block."""
-    return nn.Sequential(
-        nn.ConvTranspose2d(
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride,
-            padding,
-            output_padding,
-        ),
-        nn.BatchNorm2d(out_channels),
-        nn.LeakyReLU(),
-    )
+class Decoder(nn.Module):
+    """A simple decoder model."""
 
+    def __init__(self, hidden_dims: list[int]) -> None:
+        """Initialize the decoder.
+        Args:
+            out_channels: The number of output channels.
+            hidden_dims: The number of channels in each hidden layer.
+        Returns:
+            None
+        """
+        super().__init__()
+        modules = [
+            nn.Sequential(
+                nn.ConvTranspose2d(
+                    in_channels,
+                    out_channels,
+                    kernel_size=3,
+                    stride=2,
+                    padding=1,
+                    output_padding=1,
+                ),
+            )
+            for in_channels, out_channels in zip(hidden_dims[:-1], hidden_dims[1:])
+        ]
+        modules.append(
+            nn.Sequential(
+                nn.ConvTranspose2d(
+                    hidden_dims[-1],
+                    hidden_dims[-1],
+                    kernel_size=3,
+                    stride=2,
+                    padding=1,
+                    output_padding=1,
+                ),
+                nn.BatchNorm2d(hidden_dims[-1]),
+                nn.LeakyReLU(),
+                nn.Conv2d(hidden_dims[-1], out_channels=3, kernel_size=3, padding=1),
+                nn.Tanh(),
+            )
+        )
+        self.model = nn.Sequential(*modules)
 
-def build_final_layer(
-    in_channels: int,
-    out_channels: int,
-    kernel_size: int = 3,
-    stride: int = 2,
-    padding: int = 1,
-    output_padding: int = 1,
-) -> nn.Sequential:
-    """Build the final layer."""
-    return nn.Sequential(
-        nn.ConvTranspose2d(
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride,
-            padding,
-            output_padding,
-        ),
-        nn.BatchNorm2d(out_channels),
-        nn.LeakyReLU(),
-        nn.Conv2d(out_channels, out_channels=3, kernel_size=3, padding=1),
-        nn.Tanh(),
-    )
+    def forward(self, x):
+        """Forward pass of the decoder."""
+        x = self.model(x)
+        return x
