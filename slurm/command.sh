131c60c31a3e36c35c09bcd2b7ee008c910fcdfd
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-02-28 18:22:51 +0100

    Install packages for user only

--- slurm/command.sh
+++ slurm/command.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python -m pip install --upgrade pip
-python -m pip install -r $HOME/codis/requirements.txt
-python -m pip install -e $HOME/codis
+python -m pip install --user --upgrade pip
+python -m pip install --user --upgrade setuptools
+python -m pip install --user -r $HOME/codis/requirements.txt
+python -m pip install --user -e $HOME/codis
 python $HOME/codis/codis/train.py --dsprites_path /mnt/qb/work/bethge/dziadzio08/dsprites/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz
