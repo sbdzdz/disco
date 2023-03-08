c0e26f4f5181c6e54c1d4d69d2d930dd76c01abd
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-03-08 17:46:31 +0100

    Update command

--- slurm/command.sh
+++ slurm/command.sh
@@ -3,4 +3,4 @@ python -m pip install --user --upgrade pip
 python -m pip install --user --upgrade setuptools
 python -m pip install --user -r $HOME/codis/requirements.txt
 python -m pip install --user -e $HOME/codis
-python $HOME/codis/codis/train.py --dsprites_path /mnt/qb/work/bethge/dziadzio08/dsprites/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz --beta 1
+python $HOME/codis/codis/train.py --dsprites_path $WORK/dsprites/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz --beta 1 --epochs 1 --batch_size 128
