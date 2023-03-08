4d38885b29a0f177c051d31cdf8f595994f5316d
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-03-08 17:47:20 +0100

    Move to A100

--- slurm/run_sbatch.sh
+++ slurm/run_sbatch.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=1                                   # Number of CPU cores per task
 #SBATCH --nodes=1                                           # Ensure that all cores are on one machine
 #SBATCH --time=0-06:00                                      # Runtime in D-HH:MM
-#SBATCH --gres=gpu:rtx2080ti:1                              # Request 1 2080Ti GPU
+#SBATCH --gres=gpu:a100:1                                   # Request 1 A100 GPU
 #SBATCH --mem=50G                                           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=hostname_%j.out                            # File to which STDOUT will be written - make sure this is not on $HOME
 #SBATCH --error=hostname_%j.err                             # File to which STDERR will be written - make sure this is not on $HOME
@@ -14,4 +14,5 @@
 scontrol show job $SLURM_JOB_ID
 
 # insert your commands here
-singularity exec --nv --bind /mnt/qb/work/bethge/dziadzio08 $HOME/singularity/python_build_latest.sif $HOME/codis/slurm/command.sh
+export SINGULARITYENV_WANDB_API_KEY=$WANDB_API_KEY
+singularity exec --nv --bind $WORK $WORK/singularity/codis_latest.sif $HOME/codis/slurm/command.sh
