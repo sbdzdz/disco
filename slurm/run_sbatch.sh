28d69afb73ffe20c303f10bb7c369f89ad864ff5
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-03-09 18:28:02 +0100

    Reduce runtime

--- slurm/run_sbatch.sh
+++ slurm/run_sbatch.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1                                                        # Number of tasks (see below)
 #SBATCH --cpus-per-task=1                                                 # Number of CPU cores per task
 #SBATCH --nodes=1                                                         # Ensure that all cores are on one machine
-#SBATCH --time=0-06:00                                                    # Runtime in D-HH:MM
+#SBATCH --time=0-02:00                                                    # Runtime in D-HH:MM
 #SBATCH --gres=gpu:a100:1                                                 # Request 1 A100 GPU
 #SBATCH --mem=50G                                                         # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=/mnt/lustre/bethge/dziadzio08/logs/hostname_%j.out       # File to which STDOUT will be written - make sure this is not on $HOME
