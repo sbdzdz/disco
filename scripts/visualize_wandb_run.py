697be422277415f3548cb5487320a7be5bf4dfbe
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-03-09 19:25:06 +0100

    Fix the wandb visualization script

--- scripts/visualize_wandb_run.py
+++ scripts/visualize_wandb_run.py
@@ -12,9 +12,10 @@ def visualize_wandb_run(args):
     api = wandb.Api()
     run = api.run(f"sebastiandziadzio/codis/{args.run_id}")
     assert run.state != "running", "Run is not finished yet."
+    args.output_dir.mkdir(parents=True, exist_ok=True)
     with imageio.get_writer(args.output_dir / "training.gif", mode="I") as writer:
-        for row in run.history(pandas=False)[::10]:
-            img_path = row["reconstruction"]["path"]
+        for row in run.history(pandas=False):
+            img_path = row[args.metric_name]["path"]
             file = run.file(img_path)
             file.download(root=args.output_dir, replace=True)
             img = imageio.imread(args.output_dir / file.name)
@@ -26,6 +27,7 @@ def _main():
     parser = ArgumentParser()
     parser.add_argument("--run_id", type=str, required=True)
     parser.add_argument("--output_dir", type=Path, default=repo_root / "img/wandb")
+    parser.add_argument("--metric_name", type=str, default="reconstruction_val")
     args = parser.parse_args()
     visualize_wandb_run(args)
 
