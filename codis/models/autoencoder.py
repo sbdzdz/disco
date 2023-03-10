c25600271ad9a3bdfe18256e8388e223fdfab556
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-03-10 14:44:10 +0100

    Fix a bug

--- codis/models/autoencoder.py
+++ codis/models/autoencoder.py
@@ -18,8 +18,8 @@ class AutoEncoder(nn.Module):
             hidden_dims = [32, 64, 128, 256, 512]
         hidden_dims = [in_channels] + hidden_dims
 
-        self.encoder = Encoder(in_channels, hidden_dims)
-        self.decoder = Decoder(reversed(hidden_dims))
+        self.encoder = Encoder(hidden_dims=hidden_dims, in_channels=in_channels)
+        self.decoder = Decoder(reversed(hidden_dims), out_channels=in_channels)
 
     def forward(self, x):
         """Forward pass of the autoencoder."""
