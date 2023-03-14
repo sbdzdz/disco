c702aa1bcfabb00c94a824d52cc57246e0e6f7ce
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-03-14 12:28:26 +0100

    Reduce the number of epochs

--- slurm/command.sh
+++ slurm/command.sh
@@ -3,4 +3,4 @@ python -m pip install --user --upgrade pip
 python -m pip install --user --upgrade setuptools
 python -m pip install --user -r $HOME/codis/requirements.txt
 python -m pip install --user -e $HOME/codis
-python $HOME/codis/codis/train.py --dsprites_path $WORK/datasets/dsprites/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz --wandb_dir $WORK/logs/wandb --beta 1 --epochs 5 --batch_size 64
+python $HOME/codis/codis/train.py --dsprites_path $WORK/datasets/dsprites/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz --wandb_dir $WORK/logs/wandb --beta 1 --epochs 2 --batch_size 64
