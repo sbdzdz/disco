b426a6669b869ff79fa94f37ea862fc65be089e3
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-03-20 17:43:46 +0100

    Update log paths

--- slurm/run_sbatch.sh
+++ slurm/run_sbatch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --ntasks=1                                                                          # Number of tasks (see below)
-#SBATCH --cpus-per-task=1                                                                   # Number of CPU cores per task
-#SBATCH --nodes=1                                                                           # Ensure that all cores are on one machine
-#SBATCH --time=0-06:00                                                                      # Runtime in D-HH:MM
-#SBATCH --gres=gpu:1                                                                        # Request 1 GPU
-#SBATCH --mem=50G                                                                           # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --output=/mnt/qb/work/bethge/dziadzio08/projects/codis/logs/slurm/hostname_%j.out   # File to which STDOUT will be written - make sure this is not on $HOME
-#SBATCH --error=/mnt/qb/work/bethge/dziadzio08/projects/codis/logs/slurm/hostname_%j.err    # File to which STDERR will be written - make sure this is not on $HOME
-#SBATCH --mail-type=END,FAIL                                                                # Type of email notification- BEGIN,END,FAIL,ALL
-#SBATCH --mail-user=sebastian.dziadzio@uni-tuebingen.de                                     # Email to which notifications will be sent
+#SBATCH --ntasks=1                                                                     # Number of tasks (see below)
+#SBATCH --cpus-per-task=1                                                              # Number of CPU cores per task
+#SBATCH --nodes=1                                                                      # Ensure that all cores are on one machine
+#SBATCH --time=0-06:00                                                                 # Runtime in D-HH:MM
+#SBATCH --gres=gpu:1                                                                   # Request 1 GPU
+#SBATCH --mem=50G                                                                      # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --output=/mnt/qb/work/bethge/dziadzio08/projects/codis/slurm/hostname_%j.out   # File to which STDOUT will be written - make sure this is not on $HOME
+#SBATCH --error=/mnt/qb/work/bethge/dziadzio08/projects/codis/slurm/hostname_%j.err    # File to which STDERR will be written - make sure this is not on $HOME
+#SBATCH --mail-type=END,FAIL                                                           # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=sebastian.dziadzio@uni-tuebingen.de                                # Email to which notifications will be sent
 
 # print info about current job
 scontrol show job $SLURM_JOB_ID
