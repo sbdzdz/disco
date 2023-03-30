f99473092a5f6669bc14796538814d4200a54428
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-03-30 16:23:06 +0200

    Increase the batch size

--- slurm/run_sbatch.sh
+++ slurm/run_sbatch.sh
@@ -22,4 +22,4 @@ python -m pip install --user -e $HOME/codis
 
 python $HOME/codis/codis/train.py \
  --dsprites_path $WORK/datasets/dsprites/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz \
- --wandb_dir $WORK/projects/codis/wandb --beta 1 --epochs 20 --batch_size 64 --log_every 200
+ --wandb_dir $WORK/projects/codis/wandb --beta 1 --epochs 20 --batch_size 128 --log_every 200
