2e02959d1d7470c43cbb8baf3360a15c23154373
Sebastian <sebastian.dziadzio@gmail.com>
2023-04-03 23:15:46 +0200

    Expose the lightning modules

--- codis/models/__init__.py
+++ codis/models/__init__.py
@@ -1,4 +1,9 @@
 """Models and model blocks."""
 from codis.models.autoencoder import AutoEncoder
 from codis.models.beta_vae import BetaVAE, BaseVAE
+from codis.models.lightning_modules import (
+    CodisModel,
+    LightningBetaVAE,
+    LightningMLP,
+)
 from codis.models.mlp import MLP
