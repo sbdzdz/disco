7026998fb7dd4490ba6208d3ddde84b689da8a74
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-04-06 13:08:38 +0200

    Increase memory and workers

--- slurm/train.sh
+++ slurm/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --ntasks=1                                                                     # Number of tasks (see below)
-#SBATCH --cpus-per-task=1                                                              # Number of CPU cores per task
+#SBATCH --cpus-per-task=16                                                             # Number of CPU cores per task
 #SBATCH --nodes=1                                                                      # Ensure that all cores are on one machine
-#SBATCH --time=0-02:00                                                                 # Runtime in D-HH:MM
+#SBATCH --time=0-12:00                                                                 # Runtime in D-HH:MM
 #SBATCH --gres=gpu:1                                                                   # Request 1 GPU
-#SBATCH --mem=50G                                                                      # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --output=/mnt/lustre/bethge/dziadzio08/projects/codis/slurm/hostname_%j.out   # File to which STDOUT will be written - make sure this is not on $HOME
-#SBATCH --error=/mnt/lustre/bethge/dziadzio08/projects/codis/slurm/hostname_%j.err    # File to which STDERR will be written - make sure this is not on $HOME
+#SBATCH --mem-per-cpu=16G                                                              # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --output=/mnt/lustre/bethge/dziadzio08/projects/codis/slurm/hostname_%j.out    # File to which STDOUT will be written - make sure this is not on $HOME
+#SBATCH --error=/mnt/lustre/bethge/dziadzio08/projects/codis/slurm/hostname_%j.err     # File to which STDERR will be written - make sure this is not on $HOME
 #SBATCH --mail-type=END,FAIL                                                           # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=sebastian.dziadzio@uni-tuebingen.de                                # Email to which notifications will be sent
 
@@ -22,4 +22,4 @@ python -m pip install -e $HOME/codis
 
 python $HOME/codis/codis/train.py \
  --dsprites_path $WORK/datasets/dsprites/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz \
- --wandb_dir $WORK/projects/codis/wandb --beta 1.5 --tasks 10 --log_every_n_steps 1000 --wandb_group vae_idsprites --epochs 10 --experiment vae
+ --wandb_dir $WORK/projects/codis/wandb --beta 1.5 --tasks 10 --log_every_n_steps 100 --wandb_group vae_idsprites --epochs 20 --experiment vae
