4f9b4d6e20ba9766da389c4b4f1a4d554476a265
Sebastian <sebastian.dziadzio@gmail.com>
2023-05-19 21:25:01 +0200

    Sort imports

--- codis/models/__init__.py
+++ codis/models/__init__.py
@@ -1,4 +1,4 @@
 """Models and model blocks."""
 from codis.models.autoencoder import AutoEncoder
-from codis.models.beta_vae import BetaVAE, BaseVAE
+from codis.models.beta_vae import BaseVAE, BetaVAE
 from codis.models.mlp import MLP
