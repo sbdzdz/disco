c6df9b90045d5433cfcb17a5c4c341fdf8372d3d
Sebastian <sebastian.dziadzio@gmail.com>
2023-05-22 21:15:12 +0200

    Add a logging callback

--- codis/lightning/callbacks.py
+++ codis/lightning/callbacks.py
@@ -9,6 +9,7 @@ class VisualizationCallback(Callback):
     """Callback for visualizing VAE reconstructions."""
 
     def __init__(self, batch):
+        super().__init__()
         self.batch = batch
 
     def on_train_epoch_end(self, trainer, pl_module):
@@ -18,3 +19,13 @@ class VisualizationCallback(Callback):
         images = draw_batch_and_reconstructions(to_numpy(self.batch), to_numpy(x_hat))
         pl_module.logger.log_image("reconstructions", images=[images])
         pl_module.train()
+
+
+class LoggingCallback(Callback):
+    """Callback for additional logging."""
+
+    def on_train_epoch_start(self, trainer, pl_module):
+        print(f"Starting task {pl_module.train_task_id}...")
+
+    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
+        pl_module.log("task_id", pl_module.train_task_id)
