aa97334407933aba8d9b2362b0fbdaa05dc33c84
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-05-25 15:00:05 +0200

    Change vis frequency, add reconstructions of non-canonical shapes

--- codis/lightning/callbacks.py
+++ codis/lightning/callbacks.py
@@ -1,4 +1,5 @@
 """Lightning callbacks."""
+import lightning.pytorch as pl
 from lightning.pytorch.callbacks import Callback
 
 from codis.visualization import draw_batch_and_reconstructions
@@ -8,16 +9,38 @@ from codis.utils import to_numpy
 class VisualizationCallback(Callback):
     """Callback for visualizing VAE reconstructions."""
 
-    def __init__(self, batch):
+    def __init__(self, canonical_batch):
         super().__init__()
-        self.batch = batch
+        self._canonical_batch = canonical_batch
 
-    def on_train_epoch_end(self, trainer, pl_module):
-        """Visualize the images and reconstructions."""
+    def on_validation_epoch_end(
+        self, trainer: pl.Trainer, pl_module: pl.LightningModule
+    ) -> None:
+        self.log_reconstructions(
+            pl_module, self._canonical_batch, "reconstructions_canonical"
+        )
+
+    def on_validation_batch_end(
+        self,
+        trainer: pl.Trainer,
+        pl_module: pl.LightningModule,
+        outputs,
+        batch,
+        batch_idx: int,
+        dataloader_idx: int,
+    ):
+        """Visualize the first batch of the validation set and the reconstructions."""
+        if batch_idx == 0:
+            self.log_reconstructions(pl_module, batch, "reconstructions_validation")
+
+    @staticmethod
+    def log_reconstructions(pl_module, x, name, max_imgs=25):
+        """Log images and reconstructions"""
+        x = x[:max_imgs]
         pl_module.eval()
-        x_hat, *_ = pl_module(self.batch.to(pl_module.device))
-        images = draw_batch_and_reconstructions(to_numpy(self.batch), to_numpy(x_hat))
-        pl_module.logger.log_image("reconstructions", images=[images])
+        x_hat, *_ = pl_module(x)
+        images = draw_batch_and_reconstructions(to_numpy(x), to_numpy(x_hat))
+        pl_module.logger.log_image(name, images=[images])
         pl_module.train()
 
 
