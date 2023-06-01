db8d97b15a4ec7cc1af537673aa76ed99c98b00c
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-06-01 18:01:19 +0200

    Update the training script

--- slurm/train_regressor.sh
+++ slurm/train_regressor.sh
@@ -20,5 +20,5 @@ python -m pip install --upgrade pip setuptools
 python -m pip install -r $HOME/codis/requirements.txt
 python -m pip install -e $HOME/codis
 
-srun --gres=gpu:1 python $HOME/codis/codis/train.py --wandb_dir $WORK/projects/codis/wandb --tasks 9 --log_every_n_steps 50 \
+srun --gres=gpu:1 python $HOME/codis/codis/train.py --wandb_dir $WORK/projects/codis/wandb --tasks 9 --log_every_n_steps 1 \
 --wandb_group position_regressor --max_epochs 20 --num_workers 16 --img_size 128 --batch_size 256  --model regressor --factor_resolution 64
