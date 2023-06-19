fc67ef462362018ec6a0904f8ddd4bdb6552b48a
Sebastian <sebastian.dziadzio@gmail.com>
2023-06-19 18:05:16 +0200

    Import the new function in the module

--- codis/visualization/__init__.py
+++ codis/visualization/__init__.py
@@ -4,6 +4,7 @@ from codis.visualization.visualization import (
     draw_batch_density,
     draw_batch_and_reconstructions,
     draw_shapes,
+    draw_shapes_fill,
     draw_shapes_animated,
     draw_smooth_shapes,
     draw_analogy_task,
