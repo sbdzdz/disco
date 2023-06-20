abfa789b77a79eff2322e3a563e11aa2db533b5f
Sebastian <sebastian.dziadzio@gmail.com>
2023-06-20 15:42:18 +0200

    Remove old import

--- codis/visualization/__init__.py
+++ codis/visualization/__init__.py
@@ -4,7 +4,6 @@ from codis.visualization.visualization import (
     draw_batch_density,
     draw_batch_and_reconstructions,
     draw_shapes,
-    draw_shapes_fill,
     draw_shapes_animated,
     draw_smooth_shapes,
     draw_analogy_task,
