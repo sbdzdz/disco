b5676c26bbd957f92583eab601042393aa3d8ffc
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-07-11 17:09:18 +0200

    Add a to() method to Latents

--- codis/data/infinite_dsprites.py
+++ codis/data/infinite_dsprites.py
@@ -22,6 +22,17 @@ class Latents(BaseLatents):
     def __getitem__(self, key):
         return getattr(self, key)
 
+    def to(self, device):
+        """Move the latents to a device."""
+        return Latents(
+            color=self.color.to(device),
+            shape=self.shape.to(device),
+            scale=self.scale.to(device),
+            orientation=self.orientation.to(device),
+            position_x=self.position_x.to(device),
+            position_y=self.position_y.to(device),
+        )
+
 
 # pylint: disable=abstract-method
 class InfiniteDSprites(IterableDataset):
