670895ab1cc936c2cad09ef4be0915306d574dc5
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-08-04 14:48:17 +0200

    Split the training scripts

---
+++ slurm/train_stn_continual.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --ntasks=1                                                                     # Number of tasks (see below)
+#SBATCH --cpus-per-task=16                                                             # Number of CPU cores per task
+#SBATCH --nodes=1                                                                      # Ensure that all cores are on one machine
+#SBATCH --time=0-12:00                                                                 # Runtime in D-HH:MM
+#SBATCH --gres=gpu:1                                                                   # Request 1 GPU
+#SBATCH --mem-per-cpu=16G                                                              # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --output=/mnt/lustre/bethge/dziadzio08/projects/codis/slurm/hostname_%j.out    # File to which STDOUT will be written - make sure this is not on $HOME
+#SBATCH --error=/mnt/lustre/bethge/dziadzio08/projects/codis/slurm/hostname_%j.err     # File to which STDERR will be written - make sure this is not on $HOME
+#SBATCH --mail-type=END,FAIL                                                           # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=sebastian.dziadzio@uni-tuebingen.de                                # Email to which notifications will be sent
+
+# print info about current job
+scontrol show job $SLURM_JOB_ID
+
+source $HOME/.bashrc
+source $WORK/virtualenvs/codis/bin/activate
+
+python -m pip install --upgrade pip setuptools
+python -m pip install -r $HOME/codis/requirements.txt
+python -m pip install -e $HOME/codis
+
+srun --gres=gpu:1 python $HOME/codis/codis/train.py --wandb_dir $WORK/projects/codis/wandb --tasks 16 --log_every_n_steps 1 \
+--max_epochs 10 --num_workers 16 --img_size 128 --factor_resolution 16 --batch_size 512 --wandb_group stn_joint_simple_continual --model stn_simple --gamma 1.0 \
+--factors_to_regress orientation scale position_x position_y --training continual
