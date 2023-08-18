89f5aa4922825de9c4266ab0d1ef5fd039be3862
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-08-18 14:12:14 +0200

    Change the cli defaults

--- slurm/train_stn.sh
+++ slurm/train_stn.sh
@@ -21,5 +21,5 @@ python -m pip install -r $HOME/codis/requirements.txt
 python -m pip install -e $HOME/codis
 
 srun --gres=gpu:1 python $HOME/codis/codis/train.py --wandb_dir $WORK/projects/codis/wandb --tasks 16 --log_every_n_steps 1 \
---max_epochs 10 --num_workers 16 --img_size 128 --factor_resolution 16 --batch_size 512 --wandb_group stn_gf_continual --model stn_gf --gamma 0.0 \
+--max_epochs 5 --num_workers 16 --img_size 128 --factor_resolution 16 --batch_size 1024 --wandb_group stn_continual --model stn --gamma 0.5 \
 --factors_to_regress orientation scale position_x position_y --training continual
