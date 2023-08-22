5c068ab965b2e70bf7e3f546fc9ba6fa1a23643f
Sebastian <sebastian.dziadzio@gmail.com>
2023-08-22 20:19:12 +0200

    Move to hydra

--- codis/train.py
+++ codis/train.py
@@ -23,38 +23,39 @@ from codis.lightning.modules import (
     SpatialTransformerGF,
     SupervisedVAE,
 )
+from omegaconf import DictConfig
 
 torch.set_float32_matmul_precision("medium")
 
 
-def train(args):
+def train(cfg: DictConfig) -> None:
     """Train the model in a continual learning setting."""
-    shapes = [InfiniteDSprites().generate_shape() for _ in range(args.tasks)]
-    exemplars = generate_exemplars(shapes, img_size=args.img_size)
-    model, callbacks = build_model_and_callbacks(args, exemplars)
-    trainer = build_trainer(args, callbacks=callbacks)
+    shapes = [InfiniteDSprites().generate_shape() for _ in range(cfg.dataset.tasks)]
+    exemplars = generate_exemplars(shapes, img_size=cfg.dataset.img_size)
+    model, callbacks = build_model_and_callbacks(cfg, exemplars)
+    trainer = build_trainer(cfg, callbacks=callbacks)
 
-    if args.training == "continual":
+    if cfg.training.mode == "continual":
         test_loaders = []
         for train_task_id, (shape, exemplar) in enumerate(zip(shapes, exemplars)):
             train_loader, val_loader, test_loader = build_continual_data_loaders(
-                args, shape
+                cfg, shape
             )
             test_loaders.append(test_loader)
             model.task_id = train_task_id
             if model.has_buffer:
                 model.add_exemplar(exemplar)
             trainer.fit(model, train_loader, val_loader)
-            trainer.fit_loop.max_epochs += args.max_epochs
+            trainer.fit_loop.max_epochs += cfg.training.max_epochs
             for test_task_id, test_loader in enumerate(test_loaders):
                 model.task_id = test_task_id
                 trainer.test(model, test_loader)
-    elif args.training == "joint":
+    elif cfg.training.mode == "joint":
         model.task_id = 0
         if model.has_buffer:
             for exemplar in exemplars:
                 model.add_exemplar(exemplar)
-        train_loader, val_loader, test_loader = build_joint_data_loaders(args, shapes)
+        train_loader, val_loader, test_loader = build_joint_data_loaders(cfg, shapes)
         trainer.fit(model, train_loader, val_loader)
         trainer.test(model, test_loader)
     wandb.finish()
@@ -79,69 +80,70 @@ def generate_exemplars(shapes, img_size):
     return torch.stack([torch.from_numpy(img) for img in batch])
 
 
-def build_model_and_callbacks(args, exemplars):
+def build_model_and_callbacks(cfg: DictConfig, exemplars: list):
     """Prepare the appropriate model."""
     callbacks = [VisualizationCallback(exemplars), LoggingCallback()]
-    if args.model == "vae":
+    if cfg.model.name == "vae":
         vae = LightningBetaVAE(
-            img_size=args.img_size,
-            latent_dim=args.latent_dim,
-            beta=args.beta,
-            lr=args.lr,
+            img_size=cfg.dataset.img_size,
+            latent_dim=cfg.model.latent_dim,
+            beta=cfg.model.beta,
+            lr=cfg.training.lr,
         )
         model = SupervisedVAE(
-            vae=vae, gamma=args.gamma, factors_to_regress=args.factors_to_regress
+            vae=vae,
+            gamma=cfg.model.gamma,
+            factors_to_regress=cfg.model.factors_to_regress,
         )
-    elif args.model == "stn":
+    elif cfg.model.name == "stn":
         model = SpatialTransformer(
-            img_size=args.img_size,
-            lr=args.lr,
-            factors_to_regress=args.factors_to_regress,
-            gamma=args.gamma,
+            img_size=cfg.dataset.img_size,
+            lr=cfg.training.lr,
+            factors_to_regress=cfg.model.factors_to_regress,
+            gamma=cfg.model.gamma,
         )
-    elif args.model == "stn_gf":
+    elif cfg.model.name == "stn_gf":
         model = SpatialTransformerGF(
-            img_size=args.img_size,
-            lr=args.lr,
-            factors_to_regress=args.factors_to_regress,
-            gamma=args.gamma,
+            img_size=cfg.dataset.img_size,
+            lr=cfg.training.lr,
+            factors_to_regress=cfg.model.factors_to_regress,
+            gamma=cfg.model.gamma,
         )
-    elif args.model == "regressor":
+    elif cfg.model.name == "regressor":
         model = LatentRegressor(
-            img_size=args.img_size,
-            lr=args.lr,
-            factors_to_regress=args.factors_to_regress,
+            img_size=cfg.dataset.img_size,
+            lr=cfg.training.lr,
+            factors_to_regress=cfg.model.factors_to_regress,
         )
         callbacks = [LoggingCallback()]
     else:
-        raise ValueError(f"Unknown model {args.model}.")
+        raise ValueError(f"Unknown model {cfg.model.name}.")
     return model, callbacks
 
 
-def build_trainer(args, callbacks=None):
+def build_trainer(cfg: DictConfig, callbacks=None):
     """Configure the model trainer."""
     wandb_logger = WandbLogger(
-        project="codis", save_dir=args.wandb_dir, group=args.wandb_group
+        project="codis", save_dir=cfg.wandb.dir, group=cfg.wandb.group
     )
-    wandb_logger.experiment.config.update(args)
     if callbacks is None:
         callbacks = []
     return Trainer(
         accelerator="auto",
-        default_root_dir=args.wandb_dir,
+        default_root_dir=cfg.wandb.dir,
         devices=1,
         enable_checkpointing=False,
         enable_progress_bar=False,
-        log_every_n_steps=args.log_every_n_steps,
+        log_every_n_steps=cfg.wandb.log_every_n_steps,
         logger=wandb_logger,
-        max_epochs=args.max_epochs,
+        max_epochs=cfg.training.max_epochs,
         callbacks=callbacks,
     )
 
 
-def build_continual_data_loaders(args, shapes):
+def build_continual_data_loaders(cfg: DictConfig, shapes: list):
     """Build data loaders for a class-incremental continual learning scenario."""
-    n = args.factor_resolution
+    n = cfg.dataset.factor_resolution
     scale_range = np.linspace(0.5, 1.0, n)
     orientation_range = np.linspace(0, 2 * np.pi * (n / (n + 1)), n)
     position_x_range = np.linspace(0, 1, n)
@@ -151,47 +153,58 @@ def build_continual_data_loaders(args, shapes):
         shapes = [shapes]
 
     dataset = ContinualDSpritesMap(
-        img_size=args.img_size,
+        img_size=cfg.dataset.img_size,
         shapes=shapes,
         scale_range=scale_range,
         orientation_range=orientation_range,
         position_x_range=position_x_range,
         position_y_range=position_y_range,
     )
-    if args.train_dataset_size is not None:
-        dataset = torch.utils.data.Subset(
-            dataset, np.random.choice(len(dataset), args.train_dataset_size)
-        )
-
     train_dataset, test_dataset = random_split(dataset, [0.95, 0.05])
     val_dataset, test_dataset = random_split(test_dataset, [0.5, 0.5])
+
+    if cfg.dataset.train_dataset_size is not None:
+        dataset = torch.utils.data.Subset(
+            dataset, np.random.choice(len(dataset), cfg.dataset.train_dataset_size)
+        )
+
+    if cfg.dataset.test_dataset_size is not None:
+        test_dataset = torch.utils.data.Subset(
+            test_dataset,
+            np.random.choice(len(test_dataset), cfg.dataset.test_dataset_size),
+        )
+
     train_loader = DataLoader(
         train_dataset,
-        batch_size=args.batch_size,
-        num_workers=args.num_workers,
+        batch_size=cfg.dataset.batch_size,
+        num_workers=cfg.dataset.num_workers,
         shuffle=True,
     )
     val_loader = DataLoader(
-        val_dataset, batch_size=args.batch_size, num_workers=args.num_workers
+        val_dataset,
+        batch_size=cfg.dataset.batch_size,
+        num_workers=cfg.dataset.num_workers,
     )
     test_loader = DataLoader(
-        test_dataset, batch_size=args.batch_size, num_workers=args.num_workers
+        test_dataset,
+        batch_size=cfg.dataset.batch_size,
+        num_workers=cfg.dataset.num_workers,
     )
 
     return train_loader, val_loader, test_loader
 
 
-def build_joint_data_loaders(args, shapes):
+def build_joint_data_loaders(cfg: DictConfig, shapes):
     """Build data loaders for a joint training scenario."""
-    scale_range = np.linspace(0.5, 1.5, args.factor_resolution)
-    orientation_range = np.linspace(0, 2 * np.pi, args.factor_resolution)
-    position_x_range = np.linspace(0, 1, args.factor_resolution)
-    position_y_range = np.linspace(0, 1, args.factor_resolution)
+    scale_range = np.linspace(0.5, 1.5, cfg.dataset.factor_resolution)
+    orientation_range = np.linspace(0, 2 * np.pi, cfg.dataset.factor_resolution)
+    position_x_range = np.linspace(0, 1, cfg.dataset.factor_resolution)
+    position_y_range = np.linspace(0, 1, cfg.dataset.factor_resolution)
 
     dataset = RandomDSpritesMap(
-        img_size=args.img_size,
+        img_size=cfg.dataset.img_size,
         shapes=shapes,
-        dataset_size=args.train_dataset_size,
+        dataset_size=cfg.dataset.train_dataset_size,
         scale_range=scale_range,
         orientation_range=orientation_range,
         position_x_range=position_x_range,
@@ -200,130 +213,36 @@ def build_joint_data_loaders(args, shapes):
     train_dataset, val_dataset = random_split(dataset, [0.95, 0.05])
     train_loader = DataLoader(
         train_dataset,
-        batch_size=args.batch_size,
-        num_workers=args.num_workers,
+        batch_size=cfg.dataset.batch_size,
+        num_workers=cfg.dataset.num_workers,
         shuffle=True,
     )
     val_loader = DataLoader(
-        val_dataset, batch_size=args.batch_size, num_workers=args.num_workers
+        val_dataset,
+        batch_size=cfg.dataset.batch_size,
+        num_workers=cfg.dataset.num_workers,
     )
 
     test_shapes = [
-        InfiniteDSprites().generate_shape() for _ in range(args.num_test_shapes)
+        InfiniteDSprites().generate_shape() for _ in range(cfg.dataset.num_test_shapes)
     ]
     test_dataset = RandomDSpritesMap(
-        img_size=args.img_size,
+        img_size=cfg.dataset.img_size,
         shapes=test_shapes,
-        dataset_size=args.test_dataset_size,
+        dataset_size=cfg.dataset.test_dataset_size,
         scale_range=scale_range,
         orientation_range=orientation_range,
         position_x_range=position_x_range,
         position_y_range=position_y_range,
     )
     test_loader = DataLoader(
-        test_dataset, batch_size=args.batch_size, num_workers=args.num_workers
+        test_dataset,
+        batch_size=cfg.dataset.batch_size,
+        num_workers=cfg.dataset.num_workers,
     )
 
     return train_loader, val_loader, test_loader
 
 
-def _main():
-    parser = argparse.ArgumentParser()
-    repo_root = Path(__file__).parent.parent
-    parser.add_argument(
-        "--log_every_n_steps",
-        type=int,
-        default=50,
-        help="How often to log training progress. The metrics will be averaged.",
-    )
-    parser.add_argument(
-        "--training",
-        type=str,
-        default="continual",
-        choices=["continual", "joint"],
-        help="Training mode. One of 'continual' or 'joint'.",
-    )
-    parser.add_argument(
-        "--tasks", type=int, default=5, help="Number of continual learning tasks."
-    )
-    parser.add_argument(
-        "--num_test_shapes",
-        type=int,
-        default=1000,
-        help="Number of shapes to use for OOD testing.",
-    )
-    parser.add_argument(
-        "--train_dataset_size",
-        type=int,
-        default=None,
-        help="Number of samples to use from the training dataset. If None, use the entire dataset.",
-    )
-    parser.add_argument(
-        "--test_dataset_size",
-        type=int,
-        default=None,
-        help="Number of samples to use from the test dataset. If None, use the entire dataset.",
-    )
-    parser.add_argument(
-        "--factor_resolution",
-        type=int,
-        default=16,
-        help="Resolution of the factors of variation. The dataset size is factor_resolution ** 4.",
-    )
-    parser.add_argument(
-        "--max_epochs",
-        type=int,
-        default=1000,
-        help="Maximum number of epochs to train for.",
-    )
-    parser.add_argument("--batch_size", type=int, default=128, help="Batch size.")
-    parser.add_argument(
-        "--num_workers", type=int, default=4, help="Number of dataloader workers."
-    )
-    parser.add_argument(
-        "--img_size", type=int, default=128, help="Size of the images in the dataset."
-    )
-    parser.add_argument(
-        "--latent_dim", type=int, default=10, help="Dimensionality of the latent space."
-    )
-    parser.add_argument(
-        "--beta", type=float, default=1.0, help="Beta parameter for the beta-VAE."
-    )
-    parser.add_argument(
-        "--gamma",
-        type=float,
-        default=0.5,
-        help="Relative weight of the backbone and regressor loss. 0 is only backbone loss, 1 is only regressor loss.",
-    )
-    parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate.")
-    parser.add_argument(
-        "--wandb_dir",
-        type=Path,
-        default=repo_root / "wandb",
-        help="Wandb logging directory.",
-    )
-    parser.add_argument(
-        "--wandb_group",
-        type=str,
-        default=None,
-        help="Wandb group name. If not specified, a new group will be created.",
-    )
-    parser.add_argument(
-        "--model",
-        type=str,
-        default="stn",
-        choices=["vae", "stn", "stn_gf", "regressor"],
-        help="Model to train. One of 'vae', 'stn', 'stn_gf', or 'regressor'.",
-    )
-    parser.add_argument(
-        "--factors_to_regress",
-        type=str,
-        nargs="+",
-        default=["orientation", "scale", "position_x", "position_y"],
-    )
-    args = parser.parse_args()
-    train(args)
-
-
 if __name__ == "__main__":
-    _main()
+    train()
---
+++ slurm/train.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --ntasks=1                                                                     # Number of tasks (see below)
+#SBATCH --cpus-per-task=16                                                             # Number of CPU cores per task
+#SBATCH --nodes=1                                                                      # Ensure that all cores are on one machine
+#SBATCH --time=0-12:00                                                                 # Runtime in D-HH:MM
+#SBATCH --gres=gpu:1                                                                   # Request 1 GPU
+#SBATCH --mem-per-cpu=16G                                                              # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --output=/mnt/lustre/bethge/dziadzio08/projects/codis/slurm/hostname_%j.out    # File to which STDOUT will be written - make sure this is not on $HOME
+#SBATCH --error=/mnt/lustre/bethge/dziadzio08/projects/codis/slurm/hostname_%j.err     # File to which STDERR will be written - make sure this is not on $HOME
+#SBATCH --mail-type=END,FAIL                                                           # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=sebastian.dziadzio@uni-tuebingen.de                                # Email to which notifications will be sent
+
+# print info about current job
+scontrol show job $SLURM_JOB_ID
+
+source $HOME/.bashrc
+source $WORK/virtualenvs/codis/bin/activate
+
+python -m pip install --upgrade pip setuptools
+python -m pip install -r $HOME/codis/requirements.txt
+python -m pip install -e $HOME/codis
+
+export PYTHONPATH=$PYTHONPATH:$HOME/codis
+
+srun --gres=gpu:1 python $HOME/codis/codis/train.py --wandb_dir $WORK/projects/codis/wandb \
+dataset.tasks=16 dataset.factor_resolution=16 wandb.group=stn training.max_epochs=5 model=stn model.gamma=0.5
