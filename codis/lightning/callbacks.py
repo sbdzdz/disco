5616002561e130f0b6d3cc3ee2c3c8eac3797868
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2023-08-24 18:46:25 +0200

    Log classification

--- codis/lightning/callbacks.py
+++ codis/lightning/callbacks.py
@@ -1,5 +1,7 @@
 """Lightning callbacks."""
+from typing import Any, Optional
 import lightning.pytorch as pl
+from lightning.pytorch.utilities.types import STEP_OUTPUT
 from lightning.pytorch.callbacks import Callback
 
 from codis.visualization import draw_batch_and_reconstructions
@@ -32,6 +34,33 @@ class VisualizationCallback(Callback):
         x_hat, *_ = pl_module(x.to(pl_module.device))
         images = draw_batch_and_reconstructions(
             to_numpy(x[:max_imgs]), to_numpy(x_hat[:max_imgs])
+        )
+        pl_module.logger.log_image(name, images=[images])
+        pl_module.train()
+
+    def on_train_batch_end(
+        self,
+        trainer: pl.Trainer,
+        pl_module: pl.LightningModule,
+        outputs: Optional[STEP_OUTPUT],
+        batch: Any,
+        batch_idx: int,
+        dataloader_idx: int = 0,
+    ) -> None:
+        if batch_idx == 0:
+            self.log_classifications(pl_module, batch, "classification")
+
+    @staticmethod
+    def log_classification(pl_module, batch, name, max_imgs=25):
+        x, y = batch
+        pl_module.eval()
+        labels = pl_module.classify(x)
+        closest = [pl_module._buffer[i] for i in labels]
+        actual = [pl_module._buffer[i] for i in y.shape_ids]
+        images = draw_batch_and_reconstructions(
+            to_numpy(x[:max_imgs]),
+            to_numpy(actual[:max_imgs]),
+            to_numpy(closest[:max_imgs]),
         )
         pl_module.logger.log_image(name, images=[images])
         pl_module.train()
