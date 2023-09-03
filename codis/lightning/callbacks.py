4ee179552fda15a00efa9c27a70b740716ad761c
Sebastian <sebastian.dziadzio@gmail.com>
2023-09-03 21:59:31 +0200

    Remove task_id logging

--- codis/lightning/callbacks.py
+++ codis/lightning/callbacks.py
@@ -94,11 +94,6 @@ class VisualizationCallback(Callback):
 class LoggingCallback(Callback):
     """Callback for additional logging."""
 
-    def on_train_epoch_start(
-        self, trainer: pl.Trainer, pl_module: pl.LightningModule
-    ) -> None:
-        pl_module.log("task_id", pl_module.task_id)
-
     def on_train_start(
         self, trainer: pl.Trainer, pl_module: pl.LightningModule
     ) -> None:
