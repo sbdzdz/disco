f5479a4ef3d277bfdee54f5ee555c8173d2a79e2
Sebastian <sebastian.dziadzio@gmail.com>
2023-09-04 23:40:39 +0200

    Set x axis to show tasks instead of steps, format y axis ticks

--- scripts/visualize_loss.py
+++ scripts/visualize_loss.py
@@ -18,13 +18,14 @@ def visualize_loss(args):
     runs = api.runs(
         args.wandb_entity, filters={"group": args.wandb_group, "state": "finished"}
     )
+    num_tasks = runs[0].config["dataset.tasks"]
     if len(runs) == 0:
         raise ValueError("No matching runs found.")
 
     stages = ["val", "train"]
     if args.include_test:
         if args.test_per_task:
-            stages.extend([f"test_task_{i}" for i in range(runs[0].config["tasks"])])
+            stages.extend([f"test_task_{i}" for i in range(num_tasks)])
         else:
             stages.extend(["test"])
     plt.style.use("ggplot")
@@ -55,13 +56,16 @@ def visualize_loss(args):
                 alpha=0.3,
             )
 
-    for task_transition in get_task_transitions(runs[0]):
-        ax.axvline(task_transition, color="gray", linestyle="dotted", linewidth=1)
-
-    ax.set_xlabel("Steps")
+    # set x ticks to task transitions
+    ax.set_xticks(get_task_transitions(runs[0]))
+    ax.set_xticklabels(range(1, num_tasks))
+    ax.set_xlabel("Tasks")
     ax.set_xlim([args.xmin, args.xmax])
+
+    ax.yaxis.set_major_formatter("{x:.2f}")
     ax.set_ylabel("Loss")
     ax.set_ylim([args.ymin, args.ymax])
+
     ax.set_title(args.plot_title)
     ax.legend(loc="upper right")
 
