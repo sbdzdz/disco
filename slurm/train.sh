d6b7faad01868f14fcf087809b5589361bc5c9db
Sebastian <sebastian.dziadzio@gmail.com>
2023-09-17 13:46:09 +0200

    Change default runtime

--- slurm/train.sh
+++ slurm/train.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1                                                                     # Number of tasks (see below)
 #SBATCH --cpus-per-task=16                                                             # Number of CPU cores per task
 #SBATCH --nodes=1                                                                      # Ensure that all cores are on one machine
-#SBATCH --time=1-12:00                                                                 # Runtime in D-HH:MM
+#SBATCH --time=3-00:00                                                                 # Runtime in D-HH:MM
 #SBATCH --gres=gpu:1                                                                   # Request 1 GPU
 #SBATCH --mem-per-cpu=16G                                                              # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=/mnt/lustre/bethge/dziadzio08/projects/codis/slurm/hostname_%j.out    # File to which STDOUT will be written - make sure this is not on $HOME
