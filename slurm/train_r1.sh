1ed507c69a08cf87f75bf3985c0395e7ed5a08a5
Sebastian <sebastian.dziadzio@gmail.com>
2023-10-14 01:40:09 +0200

    Increase runtime on R1

--- slurm/train_r1.sh
+++ slurm/train_r1.sh
@@ -2,11 +2,11 @@
 #SBATCH --ntasks=1                                                                     # Number of tasks (see below)
 #SBATCH --cpus-per-task=16                                                             # Number of CPU cores per task
 #SBATCH --nodes=1                                                                      # Ensure that all cores are on one machine
-#SBATCH --time=0-12:00                                                                 # Runtime in D-HH:MM
+#SBATCH --time=2-06:00                                                                 # Runtime in D-HH:MM
 #SBATCH --gres=gpu:1                                                                   # Request 1 GPU
 #SBATCH --mem-per-cpu=16G                                                              # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --output=/mnt/qb/work/bethge/dziadzio08/projects/codis/slurm/hostname_%j.out        # File to which STDOUT will be written - make sure this is not on $HOME
-#SBATCH --error=/mnt/qb/work/bethge/dziadzio08/projects/codis/slurm/hostname_%j.err         # File to which STDERR will be written - make sure this is not on $HOME
+#SBATCH --output=/mnt/qb/work/bethge/dziadzio08/projects/codis/slurm/hostname_%j.out   # File to which STDOUT will be written - make sure this is not on $HOME
+#SBATCH --error=/mnt/qb/work/bethge/dziadzio08/projects/codis/slurm/hostname_%j.err    # File to which STDERR will be written - make sure this is not on $HOME
 #SBATCH --mail-type=END,FAIL                                                           # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=sebastian.dziadzio@uni-tuebingen.de                                # Email to which notifications will be sent
 
