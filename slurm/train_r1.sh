b11f107be8a083a884cbad128a8b603effc7fc76
Sebastian <sebastian.dziadzio@gmail.com>
2023-10-19 00:10:52 +0200

    Adjust experiment defaults

--- slurm/train_r1.sh
+++ slurm/train_r1.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1                                                                     # Number of tasks (see below)
 #SBATCH --cpus-per-task=16                                                             # Number of CPU cores per task
 #SBATCH --nodes=1                                                                      # Ensure that all cores are on one machine
-#SBATCH --time=2-06:00                                                                 # Runtime in D-HH:MM
+#SBATCH --time=3-00:00                                                                 # Runtime in D-HH:MM
 #SBATCH --gres=gpu:1                                                                   # Request 1 GPU
 #SBATCH --mem-per-cpu=16G                                                              # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=/mnt/qb/work/bethge/dziadzio08/projects/codis/slurm/hostname_%j.out   # File to which STDOUT will be written - make sure this is not on $HOME
@@ -26,5 +26,5 @@ export PYTHONPATH=$PYTHONPATH:$HOME/codis
 export HYDRA_FULL_ERROR=1
 pip install torch==2.0.1+cu117 torchvision==0.15.2+cu117 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu117
 
-srun --gres=gpu:1 python $HOME/codis/codis/train.py wandb.group=continual_classification trainer.max_epochs=4 model=stn \
- dataset.tasks=510 dataset.factor_resolution=9 dataset.shapes_per_task=10 $additional_args
+srun --gres=gpu:1 python $HOME/codis/codis/train.py wandb.group=continual_classification trainer.max_epochs=5 model=stn \
+ dataset.tasks=510 dataset.factor_resolution=8 dataset.shapes_per_task=10 $additional_args
