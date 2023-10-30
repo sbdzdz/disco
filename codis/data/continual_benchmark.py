55473296ba5f4a2ebd121ba1b4063c557a28ac6d
Sebastian <sebastian.dziadzio@gmail.com>
2023-10-30 21:22:19 +0100

    Add a balanced replay mechanism

--- codis/data/continual_benchmark.py
+++ codis/data/continual_benchmark.py
@@ -1,5 +1,5 @@
 """Class-incremental continual learning dataset."""
-from collections import defaultdict
+from collections import Counter, defaultdict
 from itertools import zip_longest
 
 import numpy as np
@@ -121,9 +121,9 @@ class ContinualBenchmarkRehearsal(ContinualBenchmark):
         super().__init__(cfg, shapes, exemplars)
 
     def __iter__(self):
-        train = None
-        val = None
-        test = None
+        train = BalancedDataset(self.train_dataset_size)
+        val = BalancedDataset(self.val_dataset_size)
+        test = BalancedDataset(self.test_dataset_size)
         for task_shapes, task_shape_ids, task_exemplars in zip(
             self.grouper(self.shapes, self.shapes_per_task),
             self.grouper(self.shape_ids, self.shapes_per_task),
@@ -132,7 +132,78 @@ class ContinualBenchmarkRehearsal(ContinualBenchmark):
             train_task, val_task, test_task = self.build_datasets(
                 task_shapes, task_shape_ids
             )
-            train = self.update_dataset(train, train_task, self.train_dataset_size)
-            val = self.update_dataset(val, val_task, self.val_dataset_size)
-            test = self.update_dataset(test, test_task, self.test_dataset_size)
+            train.update(train_task)
+            val.update(val_task)
+            test.update(test_task)
             yield (train, val, test), task_exemplars
+
+
+class BalancedDataset:
+    """Class-balanced reservoir sampling."""
+
+    def __init__(self, max_size: int) -> None:
+        """Initialize the class-balanced reservoir sampling.
+        Args:
+            max_size: The maximum size of the dataset.
+        """
+        self.max_size = max_size
+        self.dataset = None
+        self.stored_class_counts = Counter()
+        self.seen_class_counts = Counter()
+        self.full_classes = set()
+
+    def __iter__(self):
+        return iter(self.dataset)
+
+    def update(self, task_dataset: Dataset) -> None:
+        task_data = [task_dataset.dataset.data[idx] for idx in task_dataset.indices]
+        task_class_ids = [factors.shape_id for factors in task_data]
+
+        if self.dataset is None:  # empty
+            dataset = ContinualDSpritesMap(
+                img_size=self.img_size,
+                dataset_size=1,
+                shapes=self.shapes,
+                shape_ids=self.shape_ids,
+            )  # dummy dataset
+            dataset.data = task_data
+            self.stored_class_counts.update(task_class_ids)
+            self.seen_class_counts.update(task_class_ids)
+
+        elif len(self.dataset) < self.max_size:  # not full yet
+            self.dataset.data.extend(task_data)
+            self.stored_class_counts.update(task_class_ids)
+            self.seen_class_counts.update(task_class_ids)
+        else:  # full
+            for factors in task_data:
+                self.full_classes.update(self.get_largest_classes())
+                class_id = factors.shape_id
+                if class_id in self.full_classes:
+                    u = np.random.uniform()
+                    stored = self.stored_class_counts[class_id]
+                    seen = self.seen_class_counts[class_id]
+                    if u <= stored / seen:
+                        idx = self.get_random_instance(class_id)
+                        self.dataset.data[idx] = factors
+                else:
+                    largest_class = np.random.choice(self.get_largest_classes())
+                    idx = self.get_random_instance(largest_class)
+                    self.dataset.data[idx] = factors
+                    self.stored_class_counts[class_id] += 1
+                    self.stored_class_counts[largest_class] -= 1
+                self.seen_class_counts[class_id] += 1
+
+    def get_largest_classes(self):
+        """Get the most represented classes."""
+        max_count = max(self.stored_class_counts.values())
+        return [
+            class_id
+            for class_id, count in self.stored_class_counts.items()
+            if count == max_count
+        ]
+
+    def get_random_instance(self, class_id: int):
+        """Return the index of a random instance of a given class."""
+        data = self.dataset.data
+        indices = [i for i, factors in enumerate(data) if factors.shape_id == class_id]
+        return np.random.choice(indices)
