0e4166ee413716f5c5d67166bb7278ab8b754d9f
Sebastian <sebastian.dziadzio@gmail.com>
2024-01-15 18:08:17 +0100

    Add an option to draw a canonical batch

--- scripts/visualize_random_batch.py
+++ scripts/visualize_random_batch.py
@@ -5,20 +5,30 @@ from torch.utils.data import DataLoader
 
 from codis.data import RandomDSprites
 from codis.visualization import draw_batch
-import numpy as np
+import argparse
 
 
-def sample_and_visualize():
+def _main(args):
     """Sample and visualize a random batch of images from the dataset."""
-    dataset = RandomDSprites(img_size=256, scale_range=np.array([1, 2]))
+    if args.canonical:
+        dataset = RandomDSprites(
+            img_size=args.img_size,
+            scale_range=[1.0],
+            orientation_range=[0.0],
+            position_x_range=[0.5],
+            position_y_range=[0.5],
+        )
+    else:
+        dataset = RandomDSprites(img_size=256)
     dataloader = DataLoader(dataset, batch_size=16, shuffle=False)
     imgs, _ = next(iter(dataloader))
     draw_batch(imgs, path=Path("img/random_dataset.png"))
 
 
-def _main():
-    sample_and_visualize()
-
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--img_size", type=int, default=256)
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--canonical", action="store_true")
+    args = parser.parse_args()
     _main()
