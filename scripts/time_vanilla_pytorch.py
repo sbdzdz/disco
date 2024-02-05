41c6f21ec5a7f55979bdc92b6c98e8be68338f60
Sebastian <sebastian.dziadzio@gmail.com>
2024-02-05 15:57:17 +0100

    Adjust the model output dimension

--- scripts/time_vanilla_pytorch.py
+++ scripts/time_vanilla_pytorch.py
@@ -75,7 +75,8 @@ class ContinualBenchmarkDisk:
 
 
 def train(args):
-    model = timm.create_model("resnet18").to(args.device)
+    model = timm.create_model("resnet18", pretrained=False, num_classes=6)
+    model = model.to(args.device)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
     wandb.init(project="disco", config=args)
 
