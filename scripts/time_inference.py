63368c13b3b1fbb6b2f892d17332b901f373954f
Sebastian <sebastian.dziadzio@gmail.com>
2024-02-09 22:27:58 +0100

    Add a script to time resnet18 inference

---
+++ scripts/time_inference.py
@@ -0,0 +1,31 @@
+"""Load a pre-trained ResNet-18 model and time inference on a set of images."""
+
+import timm
+from disco.data import FileDataset
+from time import time
+from argparse import ArgumentParser
+from torch.utils.data import DataLoader
+
+
+def _main(args):
+    model = timm.create_model("resnet18", pretrained=True)
+    dataset = FileDataset(args.dataset_path)
+    dataloader = DataLoader(
+        dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers
+    )
+    times = []
+    for images in dataloader:
+        start = time()
+        model(images)
+        end = time()
+        times.append(end - start)
+    average = sum(times) / len(times)
+    print(f"Average inference time: {average:.2f} seconds.")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--dataset_path", type=str, help="Path to the dataset.")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size.")
+    parser.add_argument("--num_workers", type=int, default=4, help="Number of workers.")
+    _main()
