Fix an off by one bug

disco/train_from_disk.py
@@ -218,7 +218,7 @@ def test_baseline(strategy, current_task, cfg):
     """Test a standard continual learning baseline using Avalanche."""
     datasets = [
         FileDataset(Path(cfg.dataset.path) / f"task_{task}/test")
-        for task in range(current_task + 1)
+        for task in range(1, current_task + 1)
     ]
     dataset = torch.utils.data.ConcatDataset(datasets)
     dataloader = DataLoader(