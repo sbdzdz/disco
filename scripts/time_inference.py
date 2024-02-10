7f5113bddb60ea475090ff0f715b54d51fd20081
Sebastian <sebastian.dziadzio@gmail.com>
2024-02-10 17:53:26 +0100

    Change the inference profiling script to use actual test data

--- scripts/time_inference.py
+++ scripts/time_inference.py
@@ -6,23 +6,27 @@ from disco.data import FileDataset
 from time import time
 from argparse import ArgumentParser
 from torch.utils.data import DataLoader
+from pathlib import Path
 
 
 def _main(args):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = timm.create_model("resnet18", pretrained=True).to(device)
-    dataset = FileDataset(args.dataset_path)
+    datasets = [
+        FileDataset(task_dir / "test")
+        for task_dir in Path(args.dataset_path).glob("task_*")
+    ]
+    dataset = torch.utils.data.ConcatDataset(datasets)
     dataloader = DataLoader(
-        dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers
+        dataset, batch_size=args.batch_size, num_workers=args.num_workers
     )
-    times = []
+    start = time()
     for images, _ in dataloader:
-        start = time()
         model(images.to(device))
         end = time()
-        times.append(end - start)
-    average = sum(times) / len(times)
-    print(f"Average inference time: {average:.2f} seconds.")
+    end = time()
+    duration_min = (end - start) / 60
+    print(f"Inference took {duration_min:.2f} minutes.")
 
 
 if __name__ == "__main__":
@@ -30,5 +34,6 @@ if __name__ == "__main__":
     parser.add_argument("--dataset_path", type=str, help="Path to the dataset.")
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size.")
     parser.add_argument("--num_workers", type=int, default=4, help="Number of workers.")
+    parser.add_argument("--num_tasks", type=int, default=10, help="Number of tasks.")
     args = parser.parse_args()
     _main(args)
